28353dfad4f3bf02ad3521fd81ec02512595e7c5
iccanobif <d_evo_lution@hotmail.com>
2014-08-26 23:49:53 +0200

    Fixed layout in library popup

--- FrmLibrary.Designer.cs
+++ FrmLibrary.Designer.cs
@@ -48,7 +48,7 @@
             this.LstComics.FormattingEnabled = true;
             this.LstComics.Location = new System.Drawing.Point(12, 45);
             this.LstComics.Name = "LstComics";
-            this.LstComics.Size = new System.Drawing.Size(673, 407);
+            this.LstComics.Size = new System.Drawing.Size(653, 394);
             this.LstComics.TabIndex = 1;
             this.LstComics.DoubleClick += new System.EventHandler(this.LstComics_DoubleClick);
             this.LstComics.KeyUp += new System.Windows.Forms.KeyEventHandler(this.LstComics_KeyUp);
@@ -59,7 +59,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TxtFilter.Location = new System.Drawing.Point(13, 13);
             this.TxtFilter.Name = "TxtFilter";
-            this.TxtFilter.Size = new System.Drawing.Size(672, 20);
+            this.TxtFilter.Size = new System.Drawing.Size(652, 20);
             this.TxtFilter.TabIndex = 0;
             this.TxtFilter.TextChanged += new System.EventHandler(this.TxtFilter_TextChanged);
             this.TxtFilter.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtFilter_KeyDown);
@@ -67,7 +67,7 @@
             // BtnAddComic
             // 
             this.BtnAddComic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnAddComic.Location = new System.Drawing.Point(557, 492);
+            this.BtnAddComic.Location = new System.Drawing.Point(537, 479);
             this.BtnAddComic.Name = "BtnAddComic";
             this.BtnAddComic.Size = new System.Drawing.Size(59, 21);
             this.BtnAddComic.TabIndex = 5;
@@ -79,18 +79,18 @@
             // 
             this.TxtComicName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TxtComicName.Location = new System.Drawing.Point(88, 492);
+            this.TxtComicName.Location = new System.Drawing.Point(88, 479);
             this.TxtComicName.Name = "TxtComicName";
-            this.TxtComicName.Size = new System.Drawing.Size(462, 20);
+            this.TxtComicName.Size = new System.Drawing.Size(442, 20);
             this.TxtComicName.TabIndex = 4;
             // 
             // BtnOk
             // 
             this.BtnOk.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnOk.Location = new System.Drawing.Point(13, 518);
+            this.BtnOk.Location = new System.Drawing.Point(13, 505);
             this.BtnOk.Name = "BtnOk";
-            this.BtnOk.Size = new System.Drawing.Size(672, 30);
+            this.BtnOk.Size = new System.Drawing.Size(652, 30);
             this.BtnOk.TabIndex = 6;
             this.BtnOk.Text = "Ok";
             this.BtnOk.UseVisualStyleBackColor = true;
@@ -100,7 +100,7 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 495);
+            this.label1.Location = new System.Drawing.Point(12, 482);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 7;
@@ -110,7 +110,7 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 465);
+            this.label2.Location = new System.Drawing.Point(50, 452);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 8;
@@ -120,15 +120,15 @@
             // 
             this.TxtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TxtPath.Location = new System.Drawing.Point(88, 462);
+            this.TxtPath.Location = new System.Drawing.Point(88, 449);
             this.TxtPath.Name = "TxtPath";
-            this.TxtPath.Size = new System.Drawing.Size(462, 20);
+            this.TxtPath.Size = new System.Drawing.Size(442, 20);
             this.TxtPath.TabIndex = 2;
             // 
             // BtnBrowse
             // 
             this.BtnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnBrowse.Location = new System.Drawing.Point(556, 462);
+            this.BtnBrowse.Location = new System.Drawing.Point(536, 449);
             this.BtnBrowse.Name = "BtnBrowse";
             this.BtnBrowse.Size = new System.Drawing.Size(60, 24);
             this.BtnBrowse.TabIndex = 3;
@@ -137,7 +137,8 @@
             // 
             // BtnDelete
             // 
-            this.BtnDelete.Location = new System.Drawing.Point(623, 463);
+            this.BtnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnDelete.Location = new System.Drawing.Point(603, 450);
             this.BtnDelete.Name = "BtnDelete";
             this.BtnDelete.Size = new System.Drawing.Size(61, 48);
             this.BtnDelete.TabIndex = 9;
@@ -149,7 +150,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 560);
+            this.ClientSize = new System.Drawing.Size(677, 547);
             this.Controls.Add(this.BtnDelete);
             this.Controls.Add(this.BtnBrowse);
             this.Controls.Add(this.TxtPath);
